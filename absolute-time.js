4fa56dfb15c58996e2777d1af669d3b23b33f5a6
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2021-04-25 14:00:28 +0900

    fix: something

--- absolute-time.js
+++ absolute-time.js
@@ -1,7 +1,6 @@
 $(function () {
   console.log('githubility was loaded [absolute time]');
   $("relative-time").each(function () {
-  // exchange relative-time for local-time
     var datetime = $(this).attr("datetime");
     var title = $(this).attr("title");
     var text = $(this).text();
